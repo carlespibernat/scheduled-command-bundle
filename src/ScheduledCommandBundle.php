7d71a3166541fdb6202deb1ca928a1c716a6fb67
carles.pibernat <carles.pibernat@alma3d.com>
2018-08-30 11:08:10 +0200

    Add bundle class

---
+++ src/ScheduledCommandBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ScheduledCommandBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ScheduledCommandBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
