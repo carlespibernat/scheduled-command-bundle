39fa1111ddd1d87ac7b575297857553f95617f7a
carles.pibernat <carles.pibernat@alma3d.com>
2018-08-30 11:25:13 +0200

    Load bundle's services

--- src/DependencyInjection/ScheduledCommandBundleExtension.php
+++ src/DependencyInjection/ScheduledCommandBundleExtension.php
@@ -2,7 +2,9 @@
 
 namespace ScheduledCommandBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ScheduledCommandBundleExtension extends Extension
@@ -12,5 +14,9 @@ class ScheduledCommandBundleExtension extends Extension
         $bundleConfig = $this->processConfiguration(new Configuration, $configs);
 
         $container->setParameter('scheduled.command.config', $bundleConfig);
+        
+        // load bundle's services
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
